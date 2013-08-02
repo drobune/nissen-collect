215765b08b6d38d6d52d7a881ffa2a7efb8423bd
yu1ch1 <s.yuichi227@gmail.com>
2013-08-02 15:57:57 +0900

    add base-class action

---
+++ lib/nissen-collect/action.rb
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+module NissenCollect
+
+  class Action
+
+    # 環境変数を設定する
+    def initialize
+      SHOPINFO = {
+        :shopInfo=>{
+          :shopCode=> ENV['NISSEN_COLLECT_SHOPCODE'],
+          :shopPassword=> ENV['NISSEN_COLLECT_PASSWORD'],
+          :terminalId=> ENV['NISSEN_COLLECT_TERMINALID']
+        }
+      }
+    end
+
+  end
+
+end
