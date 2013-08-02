5d2923ca0e71de4313fbe6fb49aeb4c97f0a0969
yu1ch1 <s.yuichi227@gmail.com>
2013-08-02 16:34:30 +0900

    revice acrion.rb

--- lib/nissen-collect/transaction.rb
+++ lib/nissen-collect/transaction.rb
@@ -2,20 +2,23 @@
 module NissenCollect
 
   class Transaction < Action
-    attr_accessor :body
+    attr_accessor :httpInfo,:buyer,:deliveries
     @@transaction_path = 'direct/autotransaction.do'
 
     # @bodyを初期化する
     # @param [Hash] body 生成時引数
-    def initialize body=nil
-      super()
-      @body = body ? body : [@@shopInfo,HTTPINFO,BUYER,DELIVERIES].inject {|union,hash| union.merge hash}
+    def initialize
+      super
+      @httpInfo = HTTPINFO
+      @buyer = BUYER
+      @deliveries = DELIVERIES
     end
 
     # APIコールを行う
     def call
+      body = [@@shopInfo,@httpInfo,@buyer,@deliveries].inject {|union,hash| union.merge hash}
       connect = NissenCollect::Client.new
-      connect.request('get',@@transaction_path,@body)
+      connect.request('get',@@transaction_path,body)
     end
 
   end
