658d098ab398429276249f0defc4944e0b1ebe35
yu1ch1 <s.yuichi227@gmail.com>
2013-08-05 17:10:51 +0900

    revice getauthor.rb

--- lib/nissen-collect/getauthor.rb
+++ lib/nissen-collect/getauthor.rb
@@ -9,12 +9,12 @@ module NissenCollect
     # @param [Hash] body 生成時引数
     def initialize
       super
-      @buyer = Marshal.load(Marshal.dump(BUYER))
+      @transaction = Marshal.load(Marshal.dump(TRANSACTION))
     end
 
     # APIコールを行う
     def call
-      body = [@@shopInfo,@buyer].inject {|union,hash| union.merge hash}
+      body = [@@shopInfo,@transaction].inject {|union,hash| union.merge hash}
       connect = NissenCollect::Client.new
       connect.request('get',@@transaction_path,body)
     end
